e8b3a0ee3d373af5d145bd0ad86dbfa4d546dc46
Nathaniel Post <napost@lazparking.com>
2025-07-07 15:03:46 -0400

    changing react version

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import SubscriptionForm from './components/SubscriptionForm'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
   <React.StrictMode>
     <SubscriptionForm />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
